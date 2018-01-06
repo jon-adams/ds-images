81196d6898f9f5cf846738d8707a9af0b3287beb
Jon Adams <jadams@firetiger.net>
2018-01-06 18:30:38 -0500

    Enable API Gateway REGIONAL setting

--- handler.ts
+++ handler.ts
@@ -1,41 +1,39 @@
 import { APIGatewayEvent, Context, ProxyCallback, ProxyHandler, ProxyResult } from "aws-lambda";
 
 export const health: ProxyHandler = (event: APIGatewayEvent, context: Context, cb: ProxyCallback) => {
-  // find Accept content-type, defaulting to JSON
-  const defaultContentTypeJson: string = "application/json";
-  const acceptStr: string = !event.headers || event.headers.Accept == null
-    ? defaultContentTypeJson
-    : event.headers.Accept;
-  // if JSON found, use it first, then check for common text/ styles, finally default to JSON if no other type found
-  const accept: string = acceptStr === "" ||
-    acceptStr.indexOf(defaultContentTypeJson) > -1
-    ? defaultContentTypeJson
-    : acceptStr.indexOf("text/html") > -1 ? "text/html"
-    : acceptStr.indexOf("text/plain") > -1 ? "text/plain"
-    : acceptStr.indexOf("*/*") > -1 ? defaultContentTypeJson
-    : null;
+    // find Accept content-type, defaulting to JSON
+    const defaultContentTypeJson: string = "application/json";
+    const acceptStr: string = !event.headers || event.headers.Accept == null
+        ? defaultContentTypeJson
+        : event.headers.Accept;
+    // if JSON found, use it first, then check for common text/ styles, finally default to JSON if no other type found
+    const accept: string = acceptStr === "" ||
+        acceptStr.indexOf(defaultContentTypeJson) > -1
+        ? defaultContentTypeJson
+        : acceptStr.indexOf("text/html") > -1 ? "text/html"
+        : acceptStr.indexOf("text/plain") > -1 ? "text/plain"
+        : acceptStr.indexOf("*/*") > -1 ? defaultContentTypeJson
+        : null;
 
-  const response: ProxyResult = {
-      statusCode: 0,
-      body: null,
-      headers: {
-        "Cache-Control": "no-cache, no-store, must-revalidate",
-        "Content-Type": accept || defaultContentTypeJson,
-      },
-    };
-
-  if (!accept) {
-    console.warn(415, new Error("Unsupported Media Type"), acceptStr);
-    response.statusCode = 415;
-    cb(null, response);
-  } else {
-    response.statusCode = 200;
-    response.body = accept === defaultContentTypeJson ? JSON.stringify({
-        message: "Healthy!",
-        // debugging://,input: event
-        })
-        : accept === "text/html" ? "<html><head><title>DS Images</title></head><body><h1>Healthy!</h1></body></html>"
-        : "Healthy!";
-    cb(null, response);
-  }
+    if (!accept) {
+        console.warn(415, new Error("Unsupported Media Type"), acceptStr);
+        cb(null, { statusCode: 415, body: "Unsupported Media Type", headers: { "Content-Type": "text/plain" } });
+    } else {
+        const response: ProxyResult = {
+            statusCode: 200,
+            body: accept === defaultContentTypeJson ? JSON.stringify({
+                message: "Healthy!",
+                // debugging://,input: event
+                })
+                : accept === "text/html"
+                    ? "<html><head><title>DS Images</title></head><body><h1>Healthy!</h1></body></html>"
+                : "Healthy!",
+            headers: {
+                "Content-Type": accept,
+                // "healthy" check should never be cached since it is small and should be always live and non-stale
+                "Cache-Control": "no-cache, no-store, must-revalidate",
+            },
+        };
+        cb(null, response);
+    }
 };
