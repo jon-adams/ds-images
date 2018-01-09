7addfc57e83241650dabc05c96b898607f37f944
Jon Adams <jadams@firetiger.net>
2018-01-09 15:12:47 -0500

    A little abstraction

--- handler.ts
+++ handler.ts
@@ -1,7 +1,6 @@
 import { APIGatewayEvent, Context, ProxyCallback, ProxyHandler, ProxyResult } from "aws-lambda";
-import S3 = require("aws-sdk/clients/s3");
-import { GetObjectOutput } from "aws-sdk/clients/s3";
 import { AWSError } from "aws-sdk/global";
+import { imageGet, NotFoundError, S3ImageFile } from "./image";
 
 export const image: ProxyHandler = (event: APIGatewayEvent, context: Context, cb: ProxyCallback) => {
     console.log(event, event.path, event.pathParameters, event.queryStringParameters);
@@ -17,37 +16,29 @@ export const image: ProxyHandler = (event: APIGatewayEvent, context: Context, cb
         return;
     }
 
-    new S3().getObject({
-        Bucket: process.env.BUCKET,
-        Key: key,
-        }).promise()
-            .then((result: S3.GetObjectOutput) => {
-                const body = Buffer.isBuffer(result.Body)
-                    ? result.Body
-                    : new Buffer(result.Body.toString(), "binary");
-                const data = body.toString("base64");
-                const response: ProxyResult = {
-                    statusCode: 200,
-                    body: data,
-                    headers: {
-                        // "Content-Length": data.length, // already sent by AWS automatically
-                        // TODO: ContentType needs loaded from MIME and/or file extension
-                        "Content-Type": "image/png",
-                        // TODO: Switch cache settings out of DEBUG mode:
-                        "Cache-Control": "no-cache, no-store, must-revalidate",
-                        // "Cache-Control": "public, max-age=604800, immutable", // one week, hard expiration
-                    },
-                    isBase64Encoded: true,
-                };
-                cb(null, response);
-            }).catch((err: AWSError) => {
-                if (err.code === "NoSuchKey") {
-                    console.info("Missing file", key);
-                    cb(null, { statusCode: 404, body: "", headers: { "Content-Type": defaultMimeTypeForErrors } });
-                    return;
-                }
-
-                console.error(err);
-                cb(null, { statusCode: 500, body: "", headers: { "Content-Type": defaultMimeTypeForErrors } });
-            });
+    imageGet(process.env.BUCKET, dir, file, null, null)
+        .then((data: S3ImageFile) => {
+            const response: ProxyResult = {
+                statusCode: 200,
+                body: data.base64EncodedData,
+                headers: {
+                    // "Content-Length": data.length, // automatically sent by API Gateway based on `body` size
+                    "Content-Type": data.contentType || defaultMimeTypeForErrors,
+                    // TODO: Switch cache settings out of DEBUG mode:
+                    "Cache-Control": "no-cache, no-store, must-revalidate",
+                    // "Cache-Control": "public, max-age=604800, immutable", // one week, hard expiration
+                },
+                isBase64Encoded: true,
+            };
+            cb(null, response);
+        }).catch((err: NotFoundError) => {
+            console.info("Missing S3 target", key, err);
+            cb(null, { statusCode: 404, body: "", headers: { "Content-Type": defaultMimeTypeForErrors } });
+        }).catch((err: AWSError) => {
+            console.error(err.code, err);
+            cb(null, { statusCode: 500, body: "", headers: { "Content-Type": defaultMimeTypeForErrors } });
+        }).catch((err: Error) => {
+            console.error(err);
+            cb(null, { statusCode: 500, body: "", headers: { "Content-Type": defaultMimeTypeForErrors } });
+        });
 };
