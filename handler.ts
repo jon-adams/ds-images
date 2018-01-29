42febc84cb6c827475effafc57511ff155b83147
Jon Adams <jadams@firetiger.net>
2018-01-28 22:55:19 -0500

    Some abstraction; last modified header updated for production

--- handler.ts
+++ handler.ts
@@ -8,6 +8,12 @@ import { ProviderError } from "./src/ProviderError";
 // change this import to use whichever provider you are using
 import { getObject } from "./src/aws";
 
+// defaults
+const defaultMimeTypeForErrors = "image/png";
+const defaultPrimaryColor = "#ffffff";
+const defaultSecondaryColor = "#00255c";
+
+// helper functions
 function sanitizeSizeParams(value: string): number {
     return _.clamp(_.toFinite(value), 0, 1000) || 0;
 }
@@ -25,17 +31,39 @@ function sanitizeColorParams(
     return hexColorRegex(color) ? color : defValue;
 }
 
+function outputImageFile(data: ImageFile, lastModifiedUtc: string, cb: ProxyCallback): void {
+    const response: ProxyResult = {
+        statusCode: 200,
+        body: data.base64Encode(),
+        headers: {
+            // "Content-Length": data.length, // automatically sent by API Gateway based on `body` size
+            "Content-Type": defaultMimeTypeForErrors,
+            // "Cache-Control": "no-cache, no-store, must-revalidate",
+            "Cache-Control": "public, max-age=604800, immutable", // one week, hard expiration
+            "Last-Modified": lastModifiedUtc,
+        },
+        isBase64Encoded: true,
+    };
+    cb(null, response);
+}
+
+function logAndReturnError(err: Error, statusCode: number, cb: ProxyCallback) {
+    console.error(err);
+    cb(null, { statusCode, body: "", headers: { "Content-Type": defaultMimeTypeForErrors } });
+}
+
+function logAndReturnNotFound(err: Error, cb: ProxyCallback) {
+    console.info(err);
+    cb(null, { statusCode: 404, body: "", headers: { "Content-Type": defaultMimeTypeForErrors } });
+}
+
 export const image: ProxyHandler = (event: APIGatewayEvent, context: Context, cb: ProxyCallback) => {
     console.log(event, event.path, event.pathParameters, event.queryStringParameters);
-    const defaultMimeTypeForErrors = "image/png";
     const dir: string = event.pathParameters.dir;
     const file: string = event.pathParameters.file;
-    // category isn't used for the S3 path
-    const key = dir + "/" + file;
 
     if (dir === "" || file === "") {
-        console.warn("Missing path parameters", dir, file, event.path);
-        cb(null, { statusCode: 404, body: "", headers: { "Content-Type": defaultMimeTypeForErrors } });
+        logAndReturnNotFound(new Error("Missing path parameters"), cb);
         return;
     }
 
@@ -43,63 +71,33 @@ export const image: ProxyHandler = (event: APIGatewayEvent, context: Context, cb
     const height = sanitizeSizeParams(event.queryStringParameters ? event.queryStringParameters.height : "0");
 
     if (width === 0 || height === 0) {
-        console.log(
-            "Missing width or height",
-            event.queryStringParameters ? event.queryStringParameters.width : null,
-            event.queryStringParameters ? event.queryStringParameters.height : null);
-        cb(null, { statusCode: 400, body: "", headers: { "Content-Type": defaultMimeTypeForErrors } });
+        logAndReturnNotFound(new Error("Missing width or height"), cb);
         return;
     }
 
     if (file.length > 1) {
         imageGet(process.env.BUCKET, dir, file, width, height, getObject)
             .then((data: ImageFile) => {
-                const response: ProxyResult = {
-                    statusCode: 200,
-                    body: data.base64Encode(),
-                    headers: {
-                        // "Content-Length": data.length, // automatically sent by API Gateway based on `body` size
-                        "Content-Type": data.contentType || defaultMimeTypeForErrors,
-                        // TODO: Switch cache settings out of DEBUG mode:
-                        "Cache-Control": "no-cache, no-store, must-revalidate",
-                        // "Cache-Control": "public, max-age=604800, immutable", // one week, hard expiration
-                        "Last-Modified": data.lastModified.toUTCString(),
-                    },
-                    isBase64Encoded: true,
-                };
-                cb(null, response);
+                outputImageFile(data, data.lastModified.toUTCString(), cb);
             }).catch((err: NotFoundError) => {
-                console.info("Missing S3 target", key, err);
-                cb(null, { statusCode: 404, body: "", headers: { "Content-Type": defaultMimeTypeForErrors } });
+                logAndReturnNotFound(new Error("Missing S3 target"), cb);
             }).catch((err: ProviderError) => {
                 console.error(err.code, err);
-                cb(null, { statusCode: 500, body: "", headers: { "Content-Type": defaultMimeTypeForErrors } });
+                logAndReturnError(err, 500, cb);
             }).catch((err: Error) => {
-                console.error(err);
-                cb(null, { statusCode: 500, body: "", headers: { "Content-Type": defaultMimeTypeForErrors } });
+                logAndReturnError(err, 500, cb);
             });
     } else {
-        const primaryColor = sanitizeColorParams(event.queryStringParameters, "primaryColor", "#ffffff");
-        const secondaryColor = sanitizeColorParams(event.queryStringParameters, "secondaryColor", "#00255c");
+        const primaryColor = sanitizeColorParams(event.queryStringParameters, "primaryColor", defaultPrimaryColor);
+        const secondaryColor = sanitizeColorParams(
+            event.queryStringParameters,
+            "secondaryColor",
+            defaultSecondaryColor);
         letterGet(file, width, height, primaryColor, secondaryColor)
             .then((data: ImageFile) => {
-                const response: ProxyResult = {
-                    statusCode: 200,
-                    body: data.base64Encode(),
-                    headers: {
-                        // "Content-Length": data.length, // automatically sent by API Gateway based on `body` size
-                        "Content-Type": defaultMimeTypeForErrors,
-                        // TODO: Switch cache settings out of DEBUG mode:
-                        "Cache-Control": "no-cache, no-store, must-revalidate",
-                        // "Cache-Control": "public, max-age=604800, immutable", // one week, hard expiration
-                        "Last-Modified": new Date().toUTCString(),
-                    },
-                    isBase64Encoded: true,
-                };
-                cb(null, response);
+                outputImageFile(data, new Date().toUTCString(), cb);
             }).catch((err: Error) => {
-                console.error(err);
-                cb(null, { statusCode: 500, body: "", headers: { "Content-Type": defaultMimeTypeForErrors } });
+                logAndReturnError(err, 500, cb);
             });
     }
 };
